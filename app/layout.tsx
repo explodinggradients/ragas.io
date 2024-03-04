a8b435d692936e75bba87774fc2c07a4e15b61ea
coffeewithone <coffeeandone@gmail.com>
2024-03-03 18:36:53 -0800

    feat/octolane-setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./css/style.css";
 
 import { Inter } from "next/font/google";
@@ -19,6 +20,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       lang="en"
       className="scroll-smooth"
     >
+      <Script
+        id="octolane-script"
+        src="https://cdn.octolane.com/tag.js?pk=c7c9b2b863bf7eaf4e2a"
+      />
       <body className={`${inter.variable} font-inter antialiased bg-slate-900 text-slate-100 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
           {children}
